db16352c5838217bf8dbf223494f6c5afd3dcd75
Maximka6699 <ma34lom@gmail.com>
2024-10-12 00:44:14 +0300

    дабавление всех остальных таблиц и отношений

--- app/auth.py
+++ app/auth.py
@@ -4,9 +4,11 @@ from jose import JWTError, jwt
 from passlib.context import CryptContext
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
 from pydantic import BaseModel
 from .models import User
 from .rest import get_user_by_username
+from .database import get_db
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
@@ -71,7 +73,7 @@ def verify_token(token: str):
 
 # -----------------------------------------------------------------------------------------------
 # Допустим, у нас есть функция для извлечения пользователя из токена
-async def get_current_user(token: str = Depends(oauth2_scheme)):
+async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -86,7 +88,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
     except JWTError:
         raise credentials_exception
 
-    user = get_user_by_username(username=token_data.username)
+    user = get_user_by_username(db=db, username=token_data.username)
     if user is None:
         raise credentials_exception
     return user
--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, func
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, func, ForeignKey, Float
 from .database import Base
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -15,3 +16,38 @@ class User(Base):
     role = Column(String, default="user")
     loyaltylvl = Column(String, default="1")
     score = Column(Integer, default=50)
+
+class Review(Base):
+    __tablename__ = "Reviews"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey('Users.id'), index=True)
+    gas_id = Column(Integer, ForeignKey('Gases.id'), index=True)
+    text = Column(String)
+    review_date = Column(DateTime(timezone=True), default=func.now())
+    gas = relationship("Gas", back_populates="Reviews")
+
+class Gas(Base):
+    __tablename__ = "Gases"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    adress = Column(String)
+    photo = Column(String)
+    reviews = relationship("Review", back_populates="Gases")
+    fdus = relationship("FDU", back_populates="Gases")
+
+class FDU(Base):
+    __tablename__ = "FDUs"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    service_date = Column(DateTime(timezone=True), default=func.now())
+    gases = relationship("Gas", back_populates="FDUs")
+    fuels = relationship("Fuel", back_populates="FDUs")
+
+class Fuel(Base):
+    __tablename__ = "Fuels"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String)
+    price = Column(Float)
+    fdus = relationship("FDU", back_populates="Fuels")
